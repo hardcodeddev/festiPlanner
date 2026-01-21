4ac82ad9f679a2b2f3c4dc3050bb1af995e12080
Preston Fernando <201018354+hardcodeddev@users.noreply.github.com>
2026-01-21 17:53:54 -0500

    more changes yay

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       base: '/festiPlanner/',
+      build: {
+        outDir: 'docs',
+        emptyOutDir: true,
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
